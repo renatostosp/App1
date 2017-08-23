628fddf2e66a6213c8d31c79f1b95d18c165a71e
renatostosp <renato.stosp@gmail.com>
2017-08-23 14:21:11 -0300

    Atualização

--- app/src/main/java/com/example/a20141esa0415/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a20141esa0415/myapplication/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.a20141esa0415.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    String emailUsuario;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // variaveis locais ao metodo e somente podem ser visualizadas aqui
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void topClick(View v){
+        Toast.makeText(this, "Top button clicked",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user clicked the top button");
+    }
+    public void bottomClick(View v){
+        Toast.makeText(this, "Bottom button clicked",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user licked the bottom button");
+    }
+
+    public void okClick (View v){
+        editText = (EditText) findViewById(R.id.editText);
+        emailUsuario = String.valueOf(editText.getText());
+        Toast.makeText(this, "E-mail verified",
+                Toast.LENGTH_SHORT).show();
+        Log.i("info","The user verified e-mail" + emailUsuario);
     }
 }
